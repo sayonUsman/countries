f4ccaab0fb3edf7ed67031f56b7ce66863371d32
Sayon Usman <sayonusman.ma@gmail.com>
2023-02-28 19:03:26 +0300

    version 2.0

--- countries.js
+++ countries.js
@@ -16,32 +16,28 @@ const displayCountries = (countries) => {
     const countryDiv = document.createElement("div");
 
     countryDiv.innerHTML = `
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="${
-                          country.flags.svg
-                        }" class="img-fluid rounded-start rounded-end" alt="...">
-                    </div>
-
-                    <div class="col-md-8">
-                        <div class="card-body ps-5">
-                            <h1 class="card-title fw-bold">${
-                              country.name.common
-                            }</h1>
-                            <h3>Capital: ${
-                              country.capital
-                                ? country.capital[0]
-                                : "No Capital City"
-                            }</h3>
-                            <h5>Region: ${country.region}</h5>
-                            <h5>Subregion: ${country.subregion}</h5>
-                            <h5>TimeZones: ${country.timezones[0]}</h5>
-                        </div>
-                    </div>
-                </div> 
+        <div class="card h-100">
+          <div class="row">
+            <div class="col-md-4">
+              <img src="${
+                country.flags.svg
+              }" class="img-fluid rounded-start rounded-end" alt="There is a image of flag.">
             </div>
-        `;
+
+            <div class="col-md-8">
+              <div class="card-body">
+                <h1 class="card-title fw-bold">${country.name.common}</h1>
+                <h3>Capital: ${
+                  country.capital ? country.capital[0] : "No Capital City"
+                }</h3>
+                <h5>Region: ${country.region}</h5>
+                <h5>Subregion: ${country.subregion}</h5>
+                <h5>TimeZones: ${country.timezones[0]}</h5>
+              </div>
+            </div> 
+          </div> 
+        </div>
+      `;
     countriesContainer.appendChild(countryDiv);
   });
 };
